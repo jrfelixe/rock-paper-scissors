71dc84bebbf0c150ab3cbee8c4d3e7909d46572b
jrfelixe-canuo <jrfelixe-canuo@C-DEVICE>
2025-11-09 19:43:56 -0500

    Fixed code and test

--- RockPaperScissors/rps.c
+++ RockPaperScissors/rps.c
@@ -4,15 +4,33 @@
 
 const char* RockPaperScissors(const char* player1, const char* player2)
 {
+	// Validate moves
+	if (
+		(strcmp(player1, "rock") != 0 && strcmp(player1, "paper") != 0 && strcmp(player1, "scissors") != 0) ||
+		(strcmp(player2, "rock") != 0 && strcmp(player2, "paper") != 0 && strcmp(player2, "scissors") != 0)
+	)
+		return "invalid";
+
+	// Determine who wins
 	if (strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0)
 	{
 		return "Player 1";
 	}
-	return "invalid";
+	else if (strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0)
+	{
+		return "Player 1";
+	}
+	else if (strcmp(player1, player2) == 0)
+	{
+		return "Draw";
+	}
+	else
+	{
+		return "Player 2";
+	}
 }
 
 int main()
 {
-	printf("%s\n", RockPaperScissors("rock", "scissors")); // Should print "Player 1"
 	return 0;
 }
--- RockPaperScissors/rps.h
+++ RockPaperScissors/rps.h
@@ -2,4 +2,4 @@
 
 const char* RockPaperScissors(const char* player1, const char* player2);
 
-#endif // ROCKPAPERSCISSORS_RPS_H
+//#endif // ROCKPAPERSCISSORS_RPS_H
